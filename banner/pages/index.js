2ae6dde3597da599c388fc1905a05d55bf4cdd47
Serchnm <sergio.ortiz@tectijuana.edu.mx>
2021-10-19 15:05:13 -0700

    products show on admin

--- banner/pages/index.js
+++ banner/pages/index.js
@@ -1,9 +1,31 @@
-import { Heading, Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
+import { Page } from "@shopify/polaris";
+import React, { useState } from 'react'
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+function index() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  function handleProductsSelection(payload) {
+    setIsOpen(false);
+    setProducts(payload.selection);
+  }
+  return (
+    <Page
+    title="Product Selector"
+    primaryAction={{
+      content: "Select product",
+      onAction: () => setIsOpen(true)
+    }}>
+      <ResourcePicker 
+        resourceType="Product" 
+        open={isOpen}
+        onCancel={() => setIsOpen(false)}
+        onSelection={handleProductsSelection}
+       />
+       {products.map(product => (<div>{product.title}</div>))}
+    </Page>
+  )
+}
 
-export default Index;
+export default index;
+
