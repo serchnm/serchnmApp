df76dfdafa99d0112f92de26202e9f8c67a51ec7
Serchnm <sergio.ortiz@tectijuana.edu.mx>
2021-10-26 09:50:26 -0700

    more commits for progress

---
+++ banner/components/clientRouter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { withRouter } from 'next/router';
+import { ClientRouter as AppBridgeClientRouter } from '@shopify/app-bridge-react';
+
+function clientRouter(props) {
+    const {router} = props;
+    return <AppBridgeClientRouter history={router} />
+}
+
+export default withRouter(clientRouter);
--- banner/pages/_app.js
+++ banner/pages/_app.js
@@ -7,6 +7,7 @@ import { authenticatedFetch } from "@shopify/app-bridge-utils";
 import { Redirect } from "@shopify/app-bridge/actions";
 import "@shopify/polaris/dist/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
+import ClientRouter from "../components/clientRouter";
 
 
 function userLoggedInFetch(app) {
@@ -62,6 +63,7 @@ class MyApp extends App {
             forceRedirect: true,
           }}
         >
+          <ClientRouter/>
           <MyProvider Component={Component} {...pageProps} host={host}/>
         </Provider>
       </AppProvider>
